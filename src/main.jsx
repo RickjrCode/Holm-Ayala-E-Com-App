f298891632a464c4d123fa312111391e2daf1a1c
RickjrCode <youngmonk46@gmail.com>
2024-01-03 18:56:05 -0800

    components

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <BrowserRouter>
+    {" "}
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
