a7d368515ba9b06913ab914d68aabeed751d40d0
RickjrCode <youngmonk46@gmail.com>
2024-01-11 18:01:23 -0800

    style

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,36 +1,62 @@
 import { Link } from "react-router-dom";
 import drinkLogo from "../assets/bitters-drink.png";
-import React from "react";
+import React, { useState } from "react";
+import "../Hamburger.css";
+
+const Hamburger = ({ isMenuOpen, toggleMenu }) => (
+  <div className={`menu-icon ${isMenuOpen ? "open" : ""}`} onClick={toggleMenu}>
+    <div className="bar1"></div>
+    <div className="bar2"></div>
+    <div className="bar3"></div>
+  </div>
+);
 
 export default function Navigation() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
       <nav>
+        <Hamburger isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
+
         <div className="logo">
-          <h1>
-            {" "}
-            Bitter Days <img className="logo-image" src={drinkLogo} />{" "}
-          </h1>
+          <h1>BITTER DAYS</h1>
         </div>
 
-        <ul>
+        <ul className={`menu-items ${isMenuOpen ? "open" : ""}`}>
           <li>
-            <Link to={"/"}>Home</Link>
+            <Link to={"/"} onClick={toggleMenu}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link to={"/account"}>Account</Link>
+            <Link to={"/account"} onClick={toggleMenu}>
+              Account
+            </Link>
           </li>
           <li>
-            <Link to={"/login"}>Log In</Link>
+            <Link to={"/login"} onClick={toggleMenu}>
+              Log In
+            </Link>
           </li>
           <li>
-            <Link to={"/register"}>Sign Up</Link>
+            <Link to={"/register"} onClick={toggleMenu}>
+              Sign Up
+            </Link>
           </li>
           <li>
-            <Link to={"/bitters"}>Our Bitters</Link>
+            <Link to={"/bitters"} onClick={toggleMenu}>
+              Our Bitters
+            </Link>
           </li>
         </ul>
       </nav>
+
+      {/* <img id="logo-img" src={drinkLogo} alt="Drink Logo" /> */}
     </>
   );
 }
